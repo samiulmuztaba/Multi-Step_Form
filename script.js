3a98a7f4e6b9f2769e29a271b93c1025de526590
Samiul Muztaba <samiulmuztaba101@gmail.com>
2025-07-24 16:01:50 +0000

    got back to the original code :)

--- script.js
+++ script.js
@@ -1,49 +1,41 @@
-const btnPrev = document.querySelector('.button-prev')
-const btnNext = document.querySelector('.button-next')
+const btnPrev = document.querySelector('.button-prev');
+const btnNext = document.querySelector('.button-next');
 
-let stepADs = [1]
-
-const stepAD = document.getElementById('active')
+let stepADs = [1];
 
 const stepsI = [
+    document.getElementById('1'),
     document.getElementById('2'),
     document.getElementById('3'),
     document.getElementById('4')
-]
+];
 
-stepAD.style.color = "#0F121D"
-stepAD.style.backgroundColor = "#2E6BF6"
-
+// Handle NEXT click
 btnNext.addEventListener('click', () => {
-    // Add the steps active/done in stepADs
-    if (stepADs[stepADs.length - 1] + 1 < 5) {
-        stepADs.push(stepADs[stepADs.length - 1] + 1)
-        console.log(stepADs)
+    // Add next step if within range
+    if (stepADs[stepADs.length - 1] + 1 <= 4) {
+        stepADs.push(stepADs[stepADs.length - 1] + 1);
+        console.log(stepADs);
+
+        const index = stepADs[stepADs.length - 1] - 1;
+        stepsI[index].style.backgroundColor = "#2e6bf6";
+        stepsI[index].style.color = "#0F121D";
     }
+});
 
-    // Make the steps active/done colored as blue
-    let i = stepADs[stepADs.length - 1] - 2 // defining the right index for the right ID
+// Handle PREV click
+btnPrev.addEventListener('click', () => {
+    // Remove last step if above first
+    if (stepADs.length > 1) {
+        const removed = stepADs.pop();
+        console.log(stepADs);
 
-    if (Number(stepsI[i].id) == i + 2) { // get the id of the element by the index and then turn in into number(because id gives string :)
-        stepsI[i].style.backgroundColor = "#2e6bf6"
-        stepsI[i].style.color = "#0F121D"
+        const index = removed - 1;
+        stepsI[index].style.backgroundColor = "#0F121D";
+        stepsI[index].style.color = "#ffffff";
     }
+});
 
-})
-
-// OH DAMN BUGS!
-// btnPrev.addEventListener('click', () => {
-//     // Delete the steps active/done in stepADs
-//     if (stepADs[stepADs.length - 1] + 1 > 0) {
-//         stepADs.pop()
-//         console.log(stepADs)
-//     }
-
-//     // Make the steps active/done colored as gray
-//     let i = stepADs[stepADs.length - 1] - 2 // defining the right index for the right ID
-
-//     if (Number(stepsI[i].id) == i + 2) { // get the id of the element by the index and then turn in into number(because id gives string :)
-//         stepsI[i].style.backgroundColor = "#0F121D"
-//         stepsI[i].style.color = "#ffffff"
-//     }
-// })
+// Color the initial step (step 1)
+stepsI[0].style.backgroundColor = "#2e6bf6";
+stepsI[0].style.color = "#0F121D";
